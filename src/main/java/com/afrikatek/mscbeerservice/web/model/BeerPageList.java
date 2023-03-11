6e7adbf2285f2d19c74818e32d150ba7823448a9
murukai <murukai.mberi@jumpco.io>
2023-03-11 06:31:57 +0200

    JsonCreator example

--- src/main/java/com/afrikatek/mscbeerservice/web/model/BeerPageList.java
+++ src/main/java/com/afrikatek/mscbeerservice/web/model/BeerPageList.java
@@ -1,6 +1,10 @@
 package com.afrikatek.mscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -11,6 +15,21 @@ import java.util.List;
  * created on 2023/02/13 at 03:58:02
  */
 public class BeerPageList extends PageImpl<BeerDTO> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPageList(@JsonProperty("content") List<BeerDTO> content,
+                        @JsonProperty("number") int number,
+                        @JsonProperty("size") int size,
+                        @JsonProperty("totalElements") Long totalElements,
+                        @JsonProperty("pageable")JsonNode pageable,
+                        @JsonProperty("last") boolean last,
+                        @JsonProperty("totalPages") int totalPages,
+                        @JsonProperty("sort") JsonNode sort,
+                        @JsonProperty("first") boolean first,
+                        @JsonProperty("numberOfElements") int numberOfElements){
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
     public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
