113202e257d9f5a87e94d5a2e1494e7b53ebfbc2
murukai <murukai.mberi@jumpco.io>
2023-03-11 06:22:43 +0200

    Spring API docs and JsonFormatting

--- src/main/java/com/afrikatek/mscbeerservice/web/model/BeerDTO.java
+++ src/main/java/com/afrikatek/mscbeerservice/web/model/BeerDTO.java
@@ -1,5 +1,6 @@
 package com.afrikatek.mscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -26,8 +27,10 @@ public class BeerDTO {
     private Integer version;
     @Null
     private OffsetDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @NotBlank
     private String beerName;
     @NotNull
@@ -35,6 +38,7 @@ public class BeerDTO {
     @NotNull
     @Positive
     private Long upc;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
--- src/test/java/com/afrikatek/mscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/afrikatek/mscbeerservice/web/controller/BeerControllerTest.java
@@ -3,9 +3,17 @@ package com.afrikatek.mscbeerservice.web.controller;
 import com.afrikatek.mscbeerservice.web.model.BeerDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,7 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * created by murukai
  * created on 2023/02/13 at 04:19:35
  */
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.afrikatek.mscbeerservice.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -30,8 +41,13 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andDo(MockMvcRestDocumentation.document("/v1/beer", RequestDocumentation.pathParameters(
+                        RequestDocumentation.parameterWithName("beerId").description("UUID of desired beer to get.")
+                ), PayloadDocumentation.responseFields(
+                        PayloadDocumentation.fieldWithPath("id").description("Id of the beer")
+                )));
     }
 
     @Test
